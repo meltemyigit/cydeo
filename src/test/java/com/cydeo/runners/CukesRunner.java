8d4853a09d7fa0b69dce2d7f9c4fd6939f8a2104
meltemyigit <meltemyigit@gmail.com>
2023-04-18 23:34:50 +0200

    deneme

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features",
         glue     ="com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@FLEET10-1031"
+        tags = "@FLEET10-1026"
 
 )
 
